11fc0a0a2d32cccb053f627dbbf7c76e197233dc
Lisa-Phan <dhp563@my.utexas.edu>
2023-12-06 15:51:35 -0600

    cp2k job submission

--- anotherpb/run_cp2k.sh
+++ anotherpb/run_cp2k.sh
@@ -4,13 +4,28 @@ set -e
 #### PART B. Run CP2K to equilibrate system ####
 
 ##### NAME VARIABLES #####
-PARMTOP_FILE=''
-INPCRD_FILE='' 
-JOB_TITLE=''
+PARMTOP_FILE=$1
+INPCRD_FILE=$2
+JOB_TITLE=$3
+
+SCRIPT=/scratch/09069/dhp563/workflow_code_mcpb_cp2k/anotherpb/script
+TEMPLATE=/scratch/09069/dhp563/workflow_code_mcpb_cp2k/anotherpb/slurm_template 
 
 ##### MAKE DIRECTORY #####
 mkdir -p {NPT_$JOB_TITLE,NVT_$JOB_TITLE}
 
 ##### MAKE INPUT #####
-# 1. NVT input
-python3 script/make_template_cp2k.py $PARMTOP_FILE $INPCRD_FILE NVT_$JOB_TITLE/CP2K_$JOB_TITLE 
+# 1. NVT 
+# make_template_cp2k.py takes three arguments
+# $1=prmtop file
+# $2=inpcrd file
+# $3=input_file_name
+
+
+python3 $SCRIPT/make_template_cp2k.py $PARMTOP_FILE $INPCRD_FILE NPT_$JOB_TITLE/CP2K_$JOB_TITLE.inp
+
+#move template file over
+cp $TEMPLATE/cp2k_submission_temp.sh NPT_$JOB_TITLE/.
+
+#submit job
+sbatch NPT_$JOB_TITLE/cp2k_submission_temp.sh NPT_$JOB_TITLE/CP2K_$JOB_TITLE.inp
